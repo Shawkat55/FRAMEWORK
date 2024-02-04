55ca6cbdfc744087fe8523bb2f6fd445e8d8ef57
Shawkat55 <shawkata05@gmail.com>
2024-02-04 21:04:49 +0200

    new commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PORTFOLIOComponent } from './portfolio/portfolio.component';
+import { CONTACTComponent } from './contact/contact.component';
+import { CoverComponent } from './cover/cover.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"cover",pathMatch:"full"},
+  {path:"cover",component:CoverComponent},
+  {path:"Home",component:HomeComponent},
+  {path:"PORTFOLIO",component:PORTFOLIOComponent},
+  {path:"CONTACT",component:CONTACTComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { PORTFOLIOComponent } from './portfolio/portfolio.component';
+import { CONTACTComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { CoverComponent } from './cover/cover.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    PORTFOLIOComponent,
+    CONTACTComponent,
+    FooterComponent,
+    CoverComponent
   ],
   imports: [
     BrowserModule,
